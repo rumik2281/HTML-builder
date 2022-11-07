86dce839e32a9b785c3b332910f61ba4f6095495
Kiril Rumyantsev <rumyantsev@MBP-Kiril.lan>
2022-11-07 17:33:44 +0300

    feat: add 4th task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,45 @@
+const { mkdir, readdir, copyFile, access, unlink } = require('fs/promises');
+const { constants } = require('fs');
+const path = require('path');
 
+async function copy(sourceFolder, destinationFolder) {  
+  const filesInSourceFolder = await readdir(sourceFolder);  
+
+  for (let item of filesInSourceFolder) {
+    const sourceFile = path.join(sourceFolder, item);
+    const dectionationFile = path.join(destinationFolder, item);
+
+    await copyFile(sourceFile, dectionationFile);
+  }  
+}
+
+async function folderExists(destinationFolder) {
+  try {
+    await access(destinationFolder, constants.R_OK);
+    return true; 
+  } catch {
+    return false;
+  }
+}
+
+async function copyDir() {
+  const sourceFolder = path.join(__dirname, 'files');
+  const destinationFolder = path.join(__dirname, 'files-copy');
+
+  const isExist = await folderExists(destinationFolder);
+
+  if (isExist) {
+    const filesIndestinationFolder = await readdir(destinationFolder);
+    for (let item of filesIndestinationFolder) {
+      const dectionationFile = path.join(destinationFolder, item);
+      await unlink(dectionationFile);
+    }
+  
+  } 
+  
+  await mkdir(destinationFolder, {recursive: true});  
+
+  copy(sourceFolder, destinationFolder);
+}
+
+copyDir();
